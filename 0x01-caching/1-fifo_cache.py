687462b2329df45fd9de321f0a58c158038eb25e
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-07-26 17:20:54 +0300

    fifo

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" FIFO cache"""
+
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """FIFO cache class"""
+    def __init__(self):
+        """ Initialize FIFOCache """
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                # Discard the first item (FIFO)
+                oldest_key = self.queue.pop(0)
+                del self.cache_data[oldest_key]
+                print("DISCARD:", oldest_key)
+            self.cache_data[key] = item
+            self.queue.append(key)
+
+    def get(self, key):
+        """ Get an item by key """
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        return None
